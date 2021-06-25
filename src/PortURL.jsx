c466591814870ec2e2ae509fee1673c627f5966d
Alex <alextufielamigo@gmail.com>
2021-06-25 18:43:59 -0400

    tercera subida al servidor

--- src/PortURL.jsx
+++ src/PortURL.jsx
@@ -1,6 +1,8 @@
 // export const PORT_URL='http://192.168.100.6:8000/service/'
+
 // export const PORT_URL='http://192.168.100.6:8000/api/'
 // export const PORT_URL_IMAGE='http://192.168.100.6:8000/'
+
 // export const PORT_URL='http://192.168.43.56:8000/api/'
 // export const PORT_URL='https://login-crud-app.herokuapp.com/service/'
 export const PORT_URL='https://app-backend-emap-01.herokuapp.com/api/'
