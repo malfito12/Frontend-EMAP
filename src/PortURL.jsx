69333540b9dea7e1b4f67b6cc44853e3670bab58
Alex <alextufielamigo@gmail.com>
2021-11-16 00:05:19 -0400

    cambio de direccion para heroku (diseño responsive page public)

--- src/PortURL.jsx
+++ src/PortURL.jsx
@@ -6,6 +6,3 @@ export const PORT_URL=process.env.REACT_APP_API_URL
 
 // export const PORT_URL='http://192.168.43.56:8000/api/'
 // export const PORT_URL='https://login-crud-app.herokuapp.com/service/'
-
-// export const PORT_URL='https://app-backend-emap-01.herokuapp.com/api/'
-// export const PORT_URL_IMAGE='https://app-backend-emap-01.herokuapp.com/'
